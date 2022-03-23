ddafd9986dc84693995d115faf28afc1819a3394
Anotm <tomanaleksiev101@gmail.com>
2022-03-22 22:49:07 -0400

    first upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import AppIcon from './components/AppIcon'
+const App = () => {
 
-function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div class="Grid">
+        <div class="Space1"></div>
+        <div class="App1">  <AppIcon name="Adidas" /> </div>
+        <div class="App2">  <AppIcon /> </div>
+        <div class="App3"></div>
+        <div class="App4"></div>
+        <div class="App5"></div>
+        <div class="App6"></div>
+        <div class="App7"></div>
+        <div class="App8"></div>
+        <div class="App9"></div>
+        <div class="App10"></div>
+        <div class="App11"></div>
+        <div class="App12"></div>
+        <div class="App13"></div>
+        <div class="App14"></div>
+        <div class="App15"></div>
+        <div class="App16"></div>
+        <div class="App17"></div>
+        <div class="App18"></div>
+        <div class="App19"></div>
+        <div class="App20"></div>
+        <div class="App21"></div>
+        <div class="App22"></div>
+        <div class="App23"></div>
+        <div class="App24"></div>
+        <div class="Space2"></div>
+        <div class="FaveApps"><div class="grayBox"></div></div>
+      </div>
+  </main>
   );
 }
 
